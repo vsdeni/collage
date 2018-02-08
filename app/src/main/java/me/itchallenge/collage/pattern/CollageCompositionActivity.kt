ba3f7346b0918f1ea3f3d483c4cb6788feb46601
Denys Vasylenko <vs.deni@gmail.com>
2018-02-08 15:32:10 +0200

    Added CollageLayout

---
+++ app/src/main/java/me/itchallenge/collage/pattern/CollageCompositionActivity.kt
@@ -0,0 +1,15 @@
+package me.itchallenge.collage.pattern
+
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import me.itchallenge.collage.R
+
+
+class CollageCompositionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_collage_composition)
+    }
+}
